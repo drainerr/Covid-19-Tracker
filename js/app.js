b4766a37f1f05eac62d87b61abc6f69682e51fc6
GeorgeKVR <george.kvrivi@gmail.com>
2021-10-31 18:28:53 +0400

    implement searching functionality

--- js/app.js
+++ js/app.js
@@ -2,26 +2,41 @@ const countryName = document.querySelector('.country')
 const confirmed = document.querySelector('.confirmed-li')
 const deaths = document.querySelector('.deaths-li')
 const population = document.querySelector('.population-li')
+const inputElem = document.querySelector('.input')
+const searchBtn = document.querySelector('.search-btn')
 
-const renderStats = () => {
+let enteredCountry
+
+const renderStats = (country) =>{
+    fetch('https://covid-api.mmediagroup.fr/v1/cases')
+        .then(resp => resp.json())
+        .then(stats => {
+            let countryData = stats[country].All
+            countryName.innerHTML = countryData.country
+            confirmed.innerHTML = `<span class="symbol">⚪</span> Confirmed: <span class="confirmed">${countryData.confirmed}</span>`
+            deaths.innerHTML = `<span class="symbol">🔴 </span> Deaths: <span class="deaths">${countryData.deaths}</span>`
+            population.innerHTML = `<span class="symbol">👨‍👩‍👧 </span> Population: <span class="population">${countryData.population}</span>`
+        })
+        .catch(err => console.error(new Error(`'Country not found' ${err.message}`)))
+}
+
+const renderDefaultStats = () => {
     navigator.geolocation.getCurrentPosition(pos =>{        
        const {latitude: lat, longitude: lng} = pos.coords
         fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
         .then(res => res.json())
         .then(data => {
-            fetch('https://covid-api.mmediagroup.fr/v1/cases')
-            .then(resp => resp.json())
-            .then(stats => {
-                let countryData = stats[data.country].All
-                countryName.innerHTML = countryData.country
-                confirmed.innerHTML = `<span class="symbol">⚪</span> Confirmed: <span class="confirmed">${countryData.confirmed}</span>`
-                deaths.innerHTML = `<span class="symbol">🔴 </span> Deaths: <span class="deaths">${countryData.deaths}</span>`
-                population.innerHTML = `<span class="symbol">👨‍👩‍👧 </span> Population: <span class="population">${countryData.population}</span>`
-            })
+            renderStats(data.country)
         })
     })
 };
-renderStats()
+renderDefaultStats();
 
+inputElem.addEventListener('input',function(e){
+   enteredCountry = inputElem.value
+})
 
+searchBtn.addEventListener('click',()=>{
+    renderStats(enteredCountry)
+})
 
