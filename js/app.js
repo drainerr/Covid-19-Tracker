21180624bce8f5268469fe790b637a2537089e05
georgekvr <george.kvrivi@gmail.com>
2022-02-06 13:25:08 +0400

    refactor code

--- js/app.js
+++ js/app.js
@@ -27,20 +27,16 @@ const renderStats = async (country) => {
   try {
     container.insertAdjacentHTML(
       'afterbegin',
-      `<img src="../img/480px-Loader.gif" id="loadingIMG">`
+      `<img src="./img/480px-Loader.gif" id="loadingIMG">`
     );
     displayContent('none');
-    let fetchedData = false;
-    countryName.style.display = 'none';
     const fetched = await fetch('https://covid-api.mmediagroup.fr/v1/cases');
-    fetched.ok ? (fetchedData = true) : (fetchedData = false);
-    const res = await fetched.json();
-    const stats = await res;
-    let countryData = stats[country].All;
-    if (fetchedData) {
+    if (fetched.ok) {
+      const res = await fetched.json();
+      const stats = await res;
+      let countryData = stats[country].All;
       displayContent('flex');
       document.getElementById('loadingIMG').style.display = 'none';
-      countryName.style.display = 'flex';
       countryName.innerHTML = countryData.country;
       confirmed.innerHTML = `<span class="symbol">⚪</span> Confirmed: <span class="confirmed">${countryData.confirmed}</span>`;
       deaths.innerHTML = `<span class="symbol">🔴 </span> Deaths: <span class="deaths">${countryData.deaths}</span>`;
@@ -48,7 +44,7 @@ const renderStats = async (country) => {
     }
   } catch (err) {
     liveAlert(
-      `Country Not Found! <br><hr> This is probably because you misspelled the name, or the country statistics are not provided by the API we use`,
+      `Country Not Found! <br><hr> This is probably because you misspelled the name, or the country statistics are not provided by the API we use. Please reload page and try to search again`,
       'secondary'
     );
     console.error(err.message);
